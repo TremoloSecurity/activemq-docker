b166657659712aa15551d6e5a307e01c2b61ff37
Marc Boorshtein <marc.boorshtein@tremolosecurity.com>
2024-07-16 18:00:41 -0400

    bump amq, fix health check

--- health_check.sh
+++ health_check.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-curl http://127.0.0.1:8161/api/jolokia/exec/org.apache.activemq:type=Broker,brokerName=localhost,service=Health/healthStatus 2>/dev/null | jq -r '.value'
+
+
+curl -H "Origin:http://127.0.0.1" http://127.0.0.1:8161/api/jolokia/exec/org.apache.activemq:type=Broker,brokerName=localhost,service=Health/healthStatus 2>/dev/null | jq -r '.value'
