f35cc7d3e2ac828327f786f228d36d89c37f0803
Marc Boorshtein <marc.boorshtein@tremolosecurity.com>
2024-07-16 20:40:51 -0400

    copy in the file

--- start_amq.sh
+++ start_amq.sh
@@ -4,6 +4,7 @@ echo 'activemq.username=system' > /etc/amqcreds/credentials.properties
 echo "activemq.password=$AMQ_TOKEN" >> /etc/amqcreds/credentials.properties
 echo "guest.password=$AMQ_TOKEN" >> /etc/amqcreds/credentials.properties
 
+cp /etc/amqcreds/credentials.properties /usr/local/activemq/conf/credentials.properties
 
 # start AMQ
 /usr/local/activemq/bin/activemq console xbean:file:/etc/activemq/activemq.xml
