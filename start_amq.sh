ab82f4b984ecdaa2bac7b8ab06029e53232939fc
Marc Boorshtein <marc.boorshtein@tremolosecurity.com>
2024-07-16 21:09:11 -0400

    copy amq_config instead of copying into existing conf

--- start_amq.sh
+++ start_amq.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+cp /usr/local/activemq/conf/* /etc/amqcreds/
+
 echo 'activemq.username=system' > /etc/amqcreds/credentials.properties
 echo "activemq.password=$AMQ_TOKEN" >> /etc/amqcreds/credentials.properties
 echo "guest.password=$AMQ_TOKEN" >> /etc/amqcreds/credentials.properties
 
-cp /etc/amqcreds/credentials.properties /usr/local/activemq/conf/credentials.properties
+export ACTIVEMQ_HOME=/etc/amqcreds
+
+#cp /etc/amqcreds/credentials.properties /usr/local/activemq/conf/credentials.properties
 
 # start AMQ
 /usr/local/activemq/bin/activemq console xbean:file:/etc/activemq/activemq.xml
